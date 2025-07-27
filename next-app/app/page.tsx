6423a52c36c73dee8f548b066d3887d08d152e94
Kartik Tyagi <23mca063@jssaten.ac.in>
2025-07-27 14:32:03 +0530

    next.js app BE, buggy

--- next-app/app/page.tsx
+++ next-app/app/page.tsx
@@ -1,8 +1,22 @@
+import axios from 'axios'
 
-export default function Home() {
-  return (
-    <div className="flex justify-center align-top">
-      You are at '/' route
-    </div>
-  );
+async function getData(){
+  const res = await axios.get("http://localhost:3000/api/user")
+
+  return res.data;
+}
+
+//next has async components
+export default async function Home() {
+  const userData = await getData();
+  // return (
+  //   <div className="flex justify-center align-top gap-4">
+  //     <div>
+  //       Name: {userData.name}
+  //     </div>
+  //     <div>
+  //       Age: {userData.age}
+  //     </div>
+  //   </div>
+  // );
 }
